2db2587f59d6e0b82e1ab73b0a9bd9e688832a89
luohu1 <mylhmail@163.com>
2020-06-20 17:14:16 +0800

    CmdCli: A command line tool.

--- cmd/root.go
+++ cmd/root.go
@@ -1,21 +1,13 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
-func Print(s string) {
-	fmt.Println(s)
-}
-
 var rootCmd = &cobra.Command{
-	Use:   "cobra",
-	Short: "A generator for Cobra based Applications",
-	Long: `Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
+	Use:   "cmdcli",
+	Short: "A command line tool.",
+	Long:  `CmdCli is a command line tool based on go language.`,
 }
 
 func Execute() error {
--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/luohu1/cmdcli/cmd"
 )
 
 func main() {
-	fmt.Println("Go tools: CmdCli")
-
-	cmd.Print("package: common, func: Print")
-
 	cmd.Execute()
 }
