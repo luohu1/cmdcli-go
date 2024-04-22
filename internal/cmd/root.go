48c345b0646c420f0a243a47136a4919d315c079
luohu1 <mylhmail@163.com>
2024-04-23 00:52:06 +0800

    refactor: project rename to osctl

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -1,17 +1,17 @@
 package cmd
 
 import (
-	"github.com/luohu1/glctl/internal/cmd/completion"
-	"github.com/luohu1/glctl/internal/cmd/print"
-	"github.com/luohu1/glctl/internal/cmd/version"
+	"github.com/luohu1/osctl/internal/cmd/completion"
+	"github.com/luohu1/osctl/internal/cmd/print"
+	"github.com/luohu1/osctl/internal/cmd/version"
 	"github.com/spf13/cobra"
 )
 
 func NewRootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use:   "glctl",
+		Use:   "osctl",
 		Short: "Gitlab command line tool.",
-		Long:  `glctl is a command line tool to manage gitlab.`,
+		Long:  `osctl is a command line tool to manage gitlab.`,
 	}
 
 	rootCmd.AddCommand(
